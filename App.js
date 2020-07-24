db94c06b2630cad3c76fbad80e8201dcee3eb7a7
Hectic Envek <burningremedy@gmail.com>
2020-07-24 04:23:45 -0500

    Add transition animation for Deck

--- App.js
+++ App.js
@@ -20,7 +20,32 @@ function HomeStack() {
   return (
     <Stack.Navigator initialRouteName="Decks">
       <Stack.Screen name="Decks" component={DeckList} />
-      <Stack.Screen name="Deck" component={Deck} />
+      <Stack.Screen name="Deck" component={Deck} options={{
+        transitionSpec: {
+          open: {
+            animation: 'spring',
+            config: {
+              stiffness: 1000,
+              damping: 500,
+              mass: 3,
+              overshootClamping: true,
+              restDisplacementThreshold: 0.01,
+              restSpeedThreshold: 0.01,
+            },
+          },
+          close: {
+            animation: 'spring',
+            config: {
+              stiffness: 500,
+              damping: 500,
+              mass: 3,
+              overshootClamping: true,
+              restDisplacementThreshold: 0.01,
+              restSpeedThreshold: 0.01,
+            },
+          }
+        }
+      }}/>
       <Stack.Screen name="AddQuestion" component={AddQuestion} />
       <Stack.Screen name="Quiz" component={Quiz} />
       <Stack.Screen name="QuizComplete" component={QuizComplete} />
